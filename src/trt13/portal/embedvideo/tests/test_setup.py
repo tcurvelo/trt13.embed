20ab1bd89f6163d2de9fbb5c579947f4d80b5b5d
Thiago Curvelo <tcurvelo@gmail.com>
2013-04-24 18:20:49 -0300

    add more (failing) tests

--- src/trt13/portal/embedvideo/tests/test_setup.py
+++ src/trt13/portal/embedvideo/tests/test_setup.py
@@ -2,6 +2,11 @@
 import unittest
 
 from plone import api
+from plone.app.testing import login
+from plone.app.testing import logout
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME
 from trt13.portal.embedvideo.testing import TRT13_PORTAL_EMBEDVIDEO_INTEGRATION
 
 
@@ -19,3 +24,50 @@ class TestSample(unittest.TestCase):
         installed = [p['id'] for p in self.qi_tool.listInstalledProducts()]
         self.assertTrue(pid in installed,
                         'package appears not to have been installed')
+
+
+class TestContent(unittest.TestCase):
+    layer = TRT13_PORTAL_EMBEDVIDEO_INTEGRATION
+
+    def setUp(self):
+        self.portal = api.portal.get()
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        api.content.create(
+            type='Folder',
+            title='test-folder',
+            container=self.portal
+        )
+
+        setRoles(self.portal, TEST_USER_ID, ['Member'])
+        self.folder = self.portal['test-folder']
+
+    def test_adding(self):
+        api.content.create(
+            type='trt13.portal.embedvideo.video',
+            title='v1',
+            container=self.folder
+        )
+
+    #     v1 = self.folder['v1']
+    #     self.assertTrue(IEmbedVideo.providedBy(v1))
+
+    # def test_default_values_for_checkboxes(self):
+    #     self.folder.invokeFactory('trt13.portal.embedvideo.video', 'p1')
+    #     p1 = self.folder['p1']
+    #     self.assertTrue(p1.allow_anonymous)
+    #     self.assertTrue(p1.show_results)
+
+    # def test_fti(self):
+    #     fti = queryUtility(IDexterityFTI, name='trt13.portal.embedvideo.video')
+    #     self.assertNotEquals(None, fti)
+
+    # def test_schema(self):
+    #     fti = queryUtility(IDexterityFTI, name='trt13.portal.embedvideo.video')
+    #     schema = fti.lookupSchema()
+    #     self.assertEquals(IEmbedVideo, schema)
+
+    # def test_factory(self):
+    #     fti = queryUtility(IDexterityFTI, name='trt13.portal.embedvideo.video')
+    #     factory = fti.factory
+    #     new_object = createObject(factory)
+    #     self.assertTrue(IEmbedVideo.providedBy(new_object))
