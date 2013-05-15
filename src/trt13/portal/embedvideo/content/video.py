a39c310aae45dd1904f5af26dc92e0f9dbd0aa2e
Thiago Curvelo <tcurvelo@gmail.com>
2013-05-15 19:39:17 -0300

    Obtem valores default do objeto pai, caso este seja um Video tbm

--- src/trt13/portal/embedvideo/content/video.py
+++ src/trt13/portal/embedvideo/content/video.py
@@ -29,6 +29,18 @@ class IVideo(form.Schema):
     )
 
 
+@form.default_value(field=IVideo['height'])
+@form.default_value(field=IVideo['width'])
+@form.default_value(field=IVideo['url'])
+@form.default_value(field=IVideo['mimetype'])
+def defaultValue(data):
+    context = data.context
+    if IVideo.providedBy(context):
+        return getattr(context, data.field.getName())
+    else:
+        return data.field.default
+
+
 class Video(dexterity.Container):
     grok.implements(IVideo)
 
