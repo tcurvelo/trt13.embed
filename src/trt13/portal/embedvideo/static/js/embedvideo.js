ff8d3c9e26020a365ba9803647c8105d4f228685
Thiago Curvelo <tcurvelo@gmail.com>
2013-06-21 16:39:32 -0300

    Botões para vídeos alternativos utilizando o jqueryui

--- src/trt13/portal/embedvideo/static/js/embedvideo.js
+++ src/trt13/portal/embedvideo/static/js/embedvideo.js
@@ -4,15 +4,21 @@
  */
 
 $(function(){
-    $("ul#trt13_portal_embedvideo_alternatives li a").click(function(e){
-        e.preventDefault();
-        var that = $(this);
-        $('#trt13_portal_embedvideo_player').load(
-            this.href + "?ajax_load=true #trt13_portal_embedvideo_player object",
-            function(){
-                $("ul#trt13_portal_embedvideo_alternatives li a.current").removeClass("current");
-                that.addClass("current");
-            }
-        )
-    });
+    $("ul#trt13_portal_embedvideo_alternatives li a").
+        click(function(e){
+            e.preventDefault();
+            var that = $(this);
+
+            $('#trt13_portal_embedvideo_player').load(
+                this.href + "?ajax_load=true #trt13_portal_embedvideo_player object",
+                function(){
+                    $("ul#trt13_portal_embedvideo_alternatives li a.current").
+                        button("enable").removeClass("current");
+                    that.button("disable").addClass("current");;
+                }
+            );
+
+        }).
+        button().
+        filter(".current").button("disable");
 });
